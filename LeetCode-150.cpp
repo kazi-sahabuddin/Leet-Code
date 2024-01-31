aa3a548ac9c7026279417cd3b0193a5dcf89ced0
Kazi Sahabuddin Ahmed <151852294+kazi-sahabuddin@users.noreply.github.com>
2024-01-31 23:38:55 +0600

    Evaluate Reverse Polish Notation

---
+++ LeetCode-150.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> stk;
+        for(int i = 0; i < tokens.size(); i++){
+            if(tokens[i] == "+"){
+                int a = stk.top();
+                stk.pop();
+                int b = stk.top();
+                stk.pop();
+                stk.push(a+b);
+            } else if(tokens[i] == "-"){
+                int a = stk.top();
+                stk.pop();
+                int b = stk.top();
+                stk.pop();
+                stk.push(b-a);
+            } else if(tokens[i] == "*"){
+                int a = stk.top();
+                stk.pop();
+                int b = stk.top();
+                stk.pop();
+                stk.push(a*b);
+            } else if(tokens[i] == "/"){
+                float a = stk.top();
+                stk.pop();
+                float b = stk.top();
+                stk.pop();
+                stk.push(b/a);
+            } else{
+                stk.push(stoi(tokens[i]));
+            }
+        }
+        
+
+        return stk.top();
+        
+    }
+};
