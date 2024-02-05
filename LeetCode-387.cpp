8009160669b88ff269d0989345c36d5fe65ba9a7
Kazi Sahabuddin Ahmed <151852294+kazi-sahabuddin@users.noreply.github.com>
2024-02-05 07:46:18 +0600

    First Unique Character in a String

--- LeetCode-387.cpp
+++ LeetCode-387.cpp
@@ -4,12 +4,9 @@
             umap[s[i]]++;
         }
 
-
         for(int i =0;i<s.size();i++){
             if(umap[s[i]]==1)
                 return i;
         }
-
-        return -1;
-        
+        return -1;        
     }
